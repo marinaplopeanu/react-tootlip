409ec8cfb792568deb675d4e6dc8fac4372cd7dc
Marina Plopeanu <Marina.Plopeanu@fwwebbcorp.fwwebb.com>
2019-08-20 09:40:27 -0400

    first commit

--- src/composition/Tooltip.js
+++ src/composition/Tooltip.js
@@ -14,7 +14,28 @@ function Tooltip(props) {
   )
 }
 
-class TooltipClass extends React.Component {}
+class TooltipClass extends React.Component {
+  static defaultProps = {
+    color: '#01A800', // green
+  };
+  render() {
+    console.log('Using a class component!')
+    console.log(this.props)
+    return (
+      <span className='Tooltip'>
+        <span
+          className='Tooltip-content'
+          style={{ color: this.props.color }}
+        >
+          {this.props.children}
+        </span>
+        <div className='Tooltip-message'>
+          {this.props.message}
+        </div>
+      </span>
+    )
+  }
+}
 
 // we're not exporting TooltipClass yet...
-export default Tooltip;
+export default TooltipClass;
